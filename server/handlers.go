1652354088c1c9729e1ab92ac0bb738887f1dd8d
Ian Chen <chenian317@gmail.com>
2025-11-21 23:20:41 -0300

    test: testing http module

--- server/handlers.go
+++ server/handlers.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"fmt"
+	"io"
 	"net/http"
 	"sync"
 )
@@ -18,5 +20,22 @@ func CreateServer() Server {
 // Server Handlers
 
 func (s *shortenServer) CreateURL(w http.ResponseWriter, r *http.Request) {
+	// Lock connection resources
+	s.mu.Lock()
+	defer s.mu.Unlock()
 
+	if r.Method != "POST" {
+		http.Error(w, "Only POST Method allowed", http.StatusMethodNotAllowed)
+	}
+
+	// Read body content
+	defer r.Body.Close()
+	bodyBytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Bad Request", http.StatusBadRequest)
+	}
+
+	bodyString := string(bodyBytes)
+
+	fmt.Printf("Testing, '%s'\n", bodyString)
 }

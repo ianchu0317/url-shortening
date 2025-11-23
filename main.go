39ee6e6ef92b24bce5538eb9ada9ba32076f89c4
Ian Chen <chenian317@gmail.com>
2025-11-23 20:44:13 -0300

    feat: add shutdown feature to server (so it can clean up)

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"url-shortening/server"
 
 	"github.com/joho/godotenv"
@@ -22,6 +24,10 @@ func main() {
 	}
 	defer app.CloseServer()
 
+	// Set up ctrl+c shutdown
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
 	// Start server (go routine)
 	go func() {
 		http.HandleFunc("/shorten", app.CreateURL)
@@ -30,4 +36,8 @@ func main() {
 			log.Fatal("Failed starting server")
 		}
 	}()
+
+	<-c
+	log.Println("Shutting down server...")
+
 }

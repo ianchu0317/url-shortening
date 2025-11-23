50d4ec0a01a2eb592d6fc885c8a926a60197b299
Ian Chen <chenian317@gmail.com>
2025-11-23 17:23:19 -0300

    refactor: reduce query to only use one when saving url

--- server/database.go
+++ server/database.go
@@ -7,22 +7,18 @@ import "context"
 // saveShortenURL(url, shorten) takes a created url and its shorten version and save it to database.
 // If successfull it will return ResponseCreatedURLData type to return to user, if not err != nil
 func (s *shortenServer) saveShortenURL(url, shorten string) (*ResponseCreatedURLData, error) {
-	// save to database
-	_, err := s.DB.Exec(
+	// Save to database and get response info
+	var res ResponseCreatedURLData
+	err := s.DB.QueryRow(
 		context.Background(), `
 		INSERT INTO shortened (url, shortened_url)
-		VALUES ($1, $2);`,
+		VALUES ($1, $2)
+		RETURNING id, url, shortened_url, created_at, updated_at, accessed`,
 		url, shorten,
-	)
+	).Scan(&res.ID, &res.URL, &res.ShortCode, &res.CreatedAt, &res.UpdatedAt, &res.Accessed)
 	if err != nil {
-		return nil, nil
+		return nil, err
 	}
-
-	// Get response format
-	var res ResponseCreatedURLData
-
-	row := s.DB.QueryRow(context.Background(), `SELECT * FROM shortened WHERE shortened_url=$1`, shorten)
-	row.Scan(&res.ID, &res.URL, &res.ShortCode, &res.CreatedAt, &res.UpdatedAt, &res.Accessed)
 	//Scan(&res.ID, &res.URL, &res.ShortCode, &res.CreatedAt, &res.UpdatedAt)
 	return &res, nil
 }
